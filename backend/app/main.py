f0866042149f803dc464533847c69b0ea47468d9
pankajdikale <pankajdikale@gmail.com>
2026-06-24 12:50:47 +0000

    new bug fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,13 +13,13 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="EduSnap AI - Backend")
 
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Explicit origins
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Explicit origins
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Include OPTIONS for preflight
     allow_headers=["*"],
